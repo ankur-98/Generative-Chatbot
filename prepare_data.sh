0f7419f83a45e2ebeec6dbd43d504ecc31acc90e
Ankur Agarwal <ankuronetwo@gmail.com>
2023-07-27 12:31:11 -0400

    unzip data_dir

---
+++ prepare_data.sh
@@ -0,0 +1,7 @@
+# create new folder for data:
+mkdir ./data_dir
+
+# unzip datafiles
+unzip ./ijcnlp_dailydialog/train.zip -d ./data_dir
+unzip ./ijcnlp_dailydialog/validation.zip -d ./data_dir
+unzip ./ijcnlp_dailydialog/test.zip -d ./data_dir
